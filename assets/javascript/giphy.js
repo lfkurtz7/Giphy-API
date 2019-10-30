5ece7f76b513c116019054a6e92f1034036d6b1a
Leah Kurtz <lfkurtz7@gmail.com>
2019-10-29 20:01:40 -0400

    made gifs still until clicked

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,58 +1,72 @@
-var topics = ["Raccoons", "Frenchies", "Guinea-Pigs", "Turtles", "Goats", "Otters", "Elephants", "Dogs", "Sloths"];
+var topics = ["Raccoons", "French Bulldogs", "Guinea Pigs", "Turtles", "Goats", "Otters", "Elephants", "Dogs", "Sloths"];
 
+DisplayOptionButtons(topics)
 
-$("button").on("click", function(){
-    console.log("BUTTON CLICKED");
-    var x = $(this).data("search");
+$("button").on("click", HandleGIFClick)
+
+$("#add-category").on("click", HandleCategoryADD);
+
+$(document).on("click", ".gifCard", GIFmove);
+
+function HandleGIFClick() {
+    $("#GIFArea").empty();
+    var x = $(this).attr("data-search");
     console.log(x)
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+x+"&api_key=aRbD2Jz1V9nKARvMpsFPRtUxQVgaLmUH&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=aRbD2Jz1V9nKARvMpsFPRtUxQVgaLmUH&limit=10";
     console.log(queryURL);
 
-    $.ajax({url:queryURL, method: 'GET'})
-        .done(function(response){
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function (response) {
             console.log(response);
-            for (var i = 0; i<response.data.length; i++) {
-                $('#GIFArea').prepend("<p>Rating: " + response.data[i].rating + "</p>" );
-                $('#GIFArea').prepend("<img src=' " + response.data[i].images.downsized.url+"'>");
-
-
+            for (var i = 0; i < response.data.length; i++) {
+                CreateGIFCard(response.data[i])
             }
- 
- 
- 
+
         })
 
+}
+
+function CreateGIFCard(gif) {
+    var div = $("<div>").addClass("gifCard")
+    var rating = $("<p>").text("Rating: " + gif.rating)
+    var image = $("<img>").attr("src", gif.images.fixed_height_still.url)
+    image.attr("data-stored-url", gif.images.fixed_height.url)
+
+    div.append(image, rating);
+    $('#GIFArea').prepend(div);
+}
 
 
+function GIFmove() {
+    var image = $(this).children("img");
+    var currentSrc = image.attr("src");
+    var storedSrc = image.attr("data-stored-url");
+    image.attr("src", storedSrc);
+    image.attr("data-stored-url", currentSrc);
+}
 
 
-})
-
-
-
-  // This function handles events where a movie button is clicked
-  $("#add-category").on("click", function(event) {
+function HandleCategoryADD(event) {
     event.preventDefault();
-    // This line grabs the input from the textbox
-    var gif = $("#gif-input").val().trim();
+    var newCategory = $("#text-input").val().trim();
+    topics.push(newCategory);
+    DisplayOptionButtons(topics);
+    console.log(topics);
 
-    // Adding movie from the textbox to our array
-    topics.push(gif);
+}
 
-    // Calling renderButtons which handles the processing of our movie array
-    renderButtons();
-  });
+// issue with either HandleCategoryAdd or DisplayOptionsButtons--text inputs display as buttons
+// but do not function when clicked. New buttons aren't pulling info from Giphy API, just appearing 
+// on the screen. Need to figure out how to get new buttons to pull API info!!!!
 
-  // Adding a click event listener to all elements with a class of "movie-btn"
-  //$(document).on("click", ".movie-btn", displayMovieInfo);
-
-  // Calling the renderButtons function to display the intial buttons
-  renderButtons();
-
-
-
-
-
-
-
+function DisplayOptionButtons(topics) {
+    $("#buttonArea").empty();
+    for (var i=0; i < topics.length; i++) {
+        var btn = $("<button>").addClass("btn btn-outline-primary");
+        btn.attr("data-search", topics[i]);
+        btn.text(topics[i]);
+        $("#buttonArea").append(btn);
+        
+    }
+};
